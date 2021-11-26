7ee1f6d7918fb9dc3d35bf5d62eafe0dbb8f60d4
Abdul Wahid Kahar <76991016+Abdwhidd@users.noreply.github.com>
2021-11-26 23:12:51 +0800

    Add files via upload

---
+++ lib/shared/theme.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kBlueTuaColor = Color(0xff25266F);
+Color kBlueMudaColor = Color(0xff456FE7);
+Color kWhiteColor = Color(0xffFFFFFF);
+Color kYellowColor = Color(0xffFBDA4B);
+Color kWhitegreyColor = Color(0xffF1F1F1);
+Color kAbuColor = Color(0xff4B4B4B);
+Color kRedColor = Color(0xffEB5757);
+Color kBlackMudaColor = Color(0xff373639);
+Color kGreenColor = Color(0xff59C633);
+Color kGreyTuaColor = Color(0xff4F4F4F);
+Color kGreyMudaColor = Color(0xffBDBDBD);
+Color kBlackTuaColor = Color(0xff000000);
+Color kCopyrightColor = Color(0xff828282);
+Color kOrangeColor = Color(0xffFFAA33);
+
+TextStyle whiteTextStyle = GoogleFonts.nunito(
+  color : kWhiteColor
+);
+
+TextStyle blackTuaTextStyle = GoogleFonts.nunito(
+  color : kBlackTuaColor
+);
+
+TextStyle greyMudaTextStyle = GoogleFonts.nunito(
+  color : kGreyMudaColor
+);
+
+TextStyle greyTuaTextStyle = GoogleFonts.nunito(
+  color : kGreyTuaColor
+);
+
+TextStyle blueTextStyle = GoogleFonts.nunito(
+  color : kBlueTuaColor
+);
+
+TextStyle copyrightTextStyle = GoogleFonts.nunito(
+  color : kCopyrightColor
+);
+
+TextStyle abuTextStyle = GoogleFonts.nunito(
+  color : kAbuColor
+);
+
+TextStyle bluemudaTextStyle = GoogleFonts.nunito(
+  color : kBlueMudaColor
+);
+
+TextStyle yellowTextStyle = GoogleFonts.nunito(
+  color : kYellowColor
+);
+
+TextStyle redTextStyle = GoogleFonts.nunito(
+  color : kRedColor
+);
+
+TextStyle greenTextStyle = GoogleFonts.nunito(
+  color : kGreyMudaColor
+);
+
+FontWeight Light = FontWeight.w300;
+FontWeight Regular = FontWeight.w400;
+FontWeight Medium = FontWeight.w500;
+FontWeight Semibold = FontWeight.w600;
+FontWeight Bold = FontWeight.w700;
+FontWeight ExtraBold = FontWeight.w800;
+FontWeight Black = FontWeight.w900;
+
+
+
+
+
+
+
